3ad8c56267f72f5df45c20d14e3449c4a6009456
Benjamin Eberlei <kontakt@beberlei.de>
2014-01-06 23:27:52 +0100

    Fix Bundle class loading old CompilerPass.

--- src/QafooLabs/Bundle/FrameworkExtraBundle/QafooLabsFrameworkExtraBundle.php
+++ src/QafooLabs/Bundle/FrameworkExtraBundle/QafooLabsFrameworkExtraBundle.php
@@ -3,14 +3,7 @@
 namespace QafooLabs\Bundle\FrameworkExtraBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-
-use QafooLabs\Bundle\FrameworkExtraBundle\DependencyInjection\Compiler\RoutingPass;
 
 class QafooLabsFrameworkExtraBundle extends Bundle
 {
-    public function build(ContainerBuilder $builder)
-    {
-        $builder->addCompilerPass(new RoutingPass());
-    }
 }

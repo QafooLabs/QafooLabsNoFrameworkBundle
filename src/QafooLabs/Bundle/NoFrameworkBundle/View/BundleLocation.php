ebf29a75ba21228ddce5dabbfc53576f913e6f52
Benjamin Eberlei <kontakt@beberlei.de>
2018-06-17 10:00:19 +0200

    Make Symfony 4 compatible by checking for Bundle#getParent.

--- src/QafooLabs/Bundle/NoFrameworkBundle/View/BundleLocation.php
+++ src/QafooLabs/Bundle/NoFrameworkBundle/View/BundleLocation.php
@@ -17,9 +17,13 @@ class BundleLocation
     {
         $bundle = $this->getBundleForClass($className);
 
+        // Bundle::getParent was removed in Symfony 4
+        if (!method_exists($bundle, 'getParent')) {
+            return $bundle->getName();
+        }
+
         while ($bundleName = $bundle->getName()) {
-            // Bundle::getParent was removed in Symfony 4
-            if (method_exists($bundle, 'getParent') && null === $parentBundleName = $bundle->getParent()) {
+            if (null === $parentBundleName = $bundle->getParent()) {
                 $bundleName = $bundle->getName();
 
                 break;
